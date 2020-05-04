1ae328b6b77d18788872f6eacb4d22c3a0515195
Ron Apelbaum <apelbaum@freightos.com>
2020-05-04 19:37:21 +0300

    add showMore to state with hooks

--- src/ContactList/components/ContactCard.jsx
+++ src/ContactList/components/ContactCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
 import { Contact } from '../prop-types';
 import styles from './ContactCard.module.scss';
@@ -12,22 +12,34 @@ const Field = ({ label, val }) => val ?
   null
 ;
 
-const ContactCard = ({ contact }) => (
-  <Card className={styles.card}>
-    <CardContent>
-      <CardMedia
-        component="img"
-        className={styles.image}
-        image={contact.profile_image}
-      />
-      <Typography variant="h5" component="h2">
-        {contact.name}
-      </Typography>
-      <Field label="phone number" val={contact.phone} />
-      <Field label="email" val={contact.email} />
-    </CardContent>
-  </Card>
-);
+const ContactCard = ({ contact }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <Card
+      className={styles.card}
+      onMouseEnter={() => setShowMore(true)}
+      onMouseLeave={() => setShowMore(false)}
+    >
+      <CardContent>
+        <CardMedia
+          component="img"
+          className={styles.image}
+          image={contact.profile_image}
+        />
+        <Typography variant="h5" component="h2">
+          {contact.name}
+        </Typography>
+        { showMore &&
+          <React.Fragment>
+            <Field label="phone number" val={contact.phone} />
+            <Field label="email" val={contact.email} />
+          </React.Fragment>
+        }
+      </CardContent>
+    </Card>
+  );
+};
 
 ContactCard.propTypes = {
   contact: Contact.isRequired,
