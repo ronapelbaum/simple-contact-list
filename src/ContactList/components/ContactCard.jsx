411baf8ff63a7f219c1c989de3819550a02911f6
Ron Apelbaum <apelbaum@freightos.com>
2020-05-04 19:29:27 +0300

    enhance contact card

--- src/ContactList/components/ContactCard.jsx
+++ src/ContactList/components/ContactCard.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Card, CardContent } from '@material-ui/core';
+import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
 import { Contact } from '../prop-types';
 import styles from './ContactCard.module.scss';
+
+const Field = ({ label, val }) => val ? 
+  <div>
+    <span className={styles.label}>{label}:</span>
+    {val}
+  </div>
+  :
+  null
+;
 
 const ContactCard = ({ contact }) => (
   <Card className={styles.card}>
     <CardContent>
-      <div>{contact.name}</div>
-      <div>{contact.phone}</div>
+      <CardMedia
+        component="img"
+        className={styles.image}
+        image={contact.profile_image}
+      />
+      <Typography variant="h5" component="h2">
+        {contact.name}
+      </Typography>
+      <Field label="phone number" val={contact.phone} />
+      <Field label="email" val={contact.email} />
     </CardContent>
   </Card>
 );
