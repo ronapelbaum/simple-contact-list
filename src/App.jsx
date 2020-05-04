4b9b101e1c5f413fafe65787ac6143b0a6494744
Ron Apelbaum <apelbaum@freightos.com>
2020-05-04 22:46:57 +0300

    theme the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { ContactList } from './ContactList';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@material-ui/styles';
 import store from './store';
 import './common/common.module.scss';
+import { theme } from './theme';
 
 const App = () => (
   <Provider store={store}>
+    <ThemeProvider theme={theme}>
       <ContactList />
+    </ThemeProvider>
   </Provider>
 );
 
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#20B8EA',
+      contrastText: '#fff',
+    },
+  },
+});
