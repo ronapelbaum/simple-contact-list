656960c8829a260b24d78dc29c843b4013794538
Ron Apelbaum <apelbaum@freightos.com>
2020-05-06 14:24:42 +0300

    add typography to theme

--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,20 @@ const theme = createMuiTheme({
       contrastText: "#fff",
     },
   },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
 });
 
 export default theme;
